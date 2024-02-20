d82a0bbb5fd3f1e46a7f3803ff589b695207ffe8
Saurabh <saurabhthapliyal20011682@gmail.com>
2024-02-20 15:36:05 +0530

    OS Changed

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -11,9 +11,8 @@ const Button = ({label, icon}) => {
     }
   }
   return (
-    <button onClick={jumpToProducts} className=' max-sm:px-2 max-sm:py-1 sm:text-sm max-sm:h-8 max-sm:w-fit group transition-all ease-in-out duration-500 font-mono bg-blue-600 w-fit px-5 h-10 rounded-full text-white
-                        text-xs dark:bg-purple-700 dark:hover:bg-purple-500
-                        hover:bg-blue-800 flex items-center gap-2'>
+    <button onClick={jumpToProducts} className=' max-sm:px-2 max-sm:py-1 sm:text-sm max-sm:h-8 max-sm:w-fit group transition-all ease-in-out duration-500 font-mono  w-fit px-5 h-10 rounded-full text-white
+                        text-xs flex items-center gap-2  bg-gradient-to-tr from-blue-500 to-violet-500'>
         {label}
         <div className='group-hover:translate-y-2 transition-all ease-in-out duration-500'>{icon}</div>
     </button>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import {Search} from 'lucide-react'
+import { Search } from 'lucide-react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import Category from './sidebarComponents/Category';
-const Sidebar = ({ setKeyword, onSearch }) => {
+
+const Sidebar = ({ setKeyword, onSearch, priceRange, setPriceRange, category, setCategory, setSelectedSort, selectedSort }) => {
+  // State variables
   const [inputValue, setInputValue] = useState('');
 
+  // Functions
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       setKeyword(inputValue);
@@ -14,28 +19,60 @@ const Sidebar = ({ setKeyword, onSearch }) => {
   const onSearchClick = () => {
     setKeyword(inputValue);
     onSearch();
-  }
+  };
+
+  const handlePriceChange = (value) => {
+    setPriceRange(value);
+  };
+
   return (
     <div className='flex flex-col gap-6 items-center justify-center'>
-      <section className='flex justify-center items-center mt-4 gap-2 px-4'>
+      {/* Search Section */}
+      <section className='flex relative justify-center items-center mt-4 gap-2 px-4'>
         <input
           type='text'
           placeholder='Search Products....'
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyDown={handleKeyPress}
-          className='bg-gray-100 outline-none rounded-full px-2 py-2'
+          className='bg-gray-100 outline-none rounded-full pl-3 pr-10 py-2'
         />
-        <div className=' rounded-full p-2 hover:bg-gray-200'
-        onClick={onSearchClick}>
+        <div className='rounded-full absolute right-4 p-2 hover:bg-gray-200' onClick={onSearchClick}>
           <Search className='text-blue-500 h-full cursor-pointer' size={25} />
         </div>
-
       </section>
-      <section className='dark:text-white flex flex-col gap-3'>
-        <p className='font-mono'>Filters</p>
+
+      {/* Price Range Section */}
+      <section className='flex flex-col gap-3 w-full'>
+        <div className='px-4'>
+        <div className='flex justify-between '>
+            <span>₹{priceRange[0]}</span>
+            <span>₹{priceRange[1]}</span>
+          </div>
+          <Slider
+            trackStyle={{ backgroundColor: "blue" }}
+            handleStyle={{
+              borderColor: "blue",
+              backgroundColor: "blue"
+            }}
+            range
+            min={0}
+            max={1000}
+            defaultValue={priceRange}
+            onChange={handlePriceChange}
+          />
+        </div>
+      </section>
+
+      {/* Filters Section */}
+      <section className='dark:text-white flex flex-col gap-3 w-full'>
         <div>
-          <Category/>
+          <Category
+            setSelectedSort={setSelectedSort}
+            selectedSort={selectedSort}
+            selectedCategory={category}
+            setSelectedCategory={setCategory}
+          />
         </div>
       </section>
     </div>
